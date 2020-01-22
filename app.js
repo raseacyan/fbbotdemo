08cb87d046fa84bb71bcac143b81a7cbf4be0425
Caesar <caesar@example.com>
2020-01-22 09:56:25 +0630

    test'

--- app.js
+++ app.js
@@ -104,7 +104,13 @@ function handleMessage(sender_psid, received_message) {
   let response;
   
   // Checks if the message contains text
-  if (received_message.text) {    
+  if (received_message.text == "hi") {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `Min Ga Lar Par Sint!`
+    }
+  }else if (received_message.text) {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
