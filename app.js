1055920769d9de0011994a067de26c536d4c44b7
Caesar <caesar@example.com>
2020-01-22 13:13:14 +0630

    test

--- app.js
+++ app.js
@@ -212,6 +212,30 @@ function callSendAPI(sender_psid, response) {
 
 function setupGetStartedButton(res){
         var messageData = {
+                "get_started": {"payload": "USER_DEFINED_PAYLOAD"}                
+        };
+        
+        // Start the request
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+ PAGE_ACCESS_TOKEN,
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            form: messageData
+        },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                // Print out the response body
+                res.send(body);
+
+            } else { 
+                // TODO: Handle errors
+                res.send(body);
+            }
+        });
+    } 
+
+function setupPersistentMenu(res){
+        var messageData = {
                 "psid": sender_psid,
                 "persistent_menu": [
                   {
@@ -237,29 +261,6 @@ function setupGetStartedButton(res){
                       ]
                   }
               ]               
-        };
-        // Start the request
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+ PAGE_ACCESS_TOKEN,
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            form: messageData
-        },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                // Print out the response body
-                res.send(body);
-
-            } else { 
-                // TODO: Handle errors
-                res.send(body);
-            }
-        });
-    } 
-
-function setupPersistentMenu(res){
-        var messageData = {
-                "get_started": {"payload": "USER_DEFINED_PAYLOAD"}                
         };
         // Start the request
         request({
