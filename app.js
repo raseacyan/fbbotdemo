e00dd303a2cbb5a4ae02a373832da24985d06124
Caesar <caesar@example.com>
2020-01-22 13:41:23 +0630

    test

--- app.js
+++ app.js
@@ -226,3 +226,59 @@ function setupGetStartedButton(res){
             }
         });
     } 
+
+function setupPersistentMenu(res){
+        var messageData = {
+            "persistent_menu":[
+            {
+            "locale":"default",
+            "composer_input_disabled":true,
+            "call_to_actions":[
+                {
+                "title":"Info",
+                "type":"nested",
+                "call_to_actions":[
+                    {
+                    "title":"Help",
+                    "type":"postback",
+                    "payload":"HELP_PAYLOAD"
+                    },
+                    {
+                    "title":"Contact Me",
+                    "type":"postback",
+                    "payload":"CONTACT_INFO_PAYLOAD"
+                    }
+                ]
+                },
+                {
+                "type":"web_url",
+                "title":"Visit website ",
+                "url":"http://www.techiediaries.com",
+                "webview_height_ratio":"full"
+                }
+            ]
+            },
+            {
+            "locale":"zh_CN",
+            "composer_input_disabled":false
+            }
+        ]                
+        };
+        // Start the request
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+ PAGE_ACCESS_TOKEN,
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            form: messageData
+        },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                // Print out the response body
+                res.send(body);
+
+            } else { 
+                // TODO: Handle errors
+                res.send(body);
+            }
+        });
+    } 
