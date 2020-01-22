ebfa3bfcdb23f80faac6dce053e940a3a0c2655f
Caesar <caesar@example.com>
2020-01-22 15:28:04 +0630

    test

--- app.js
+++ app.js
@@ -240,40 +240,40 @@ function setupGetStartedButton(res){
 
 function setupPersistentMenu(res){
         var messageData = { 
-              "persistent_menu":[
-    {
-    "locale":"default",
-    "composer_input_disabled":true,
-    "call_to_actions":[
-        {
-        "title":"Info",
-        "type":"nested",
-        "call_to_actions":[
-            {
-            "title":"Help",
-            "type":"postback",
-            "payload":"HELP_PAYLOAD"
+            "persistent_menu":[
+                {
+                  "locale":"default",
+                  "composer_input_disabled":false,
+                  "call_to_actions":[
+                      {
+                        "title":"Info",
+                        "type":"nested",
+                        "call_to_actions":[
+                            {
+                              "title":"Help",
+                              "type":"postback",
+                              "payload":"HELP_PAYLOAD"
+                            },
+                            {
+                              "title":"Contact Me",
+                              "type":"postback",
+                              "payload":"CONTACT_INFO_PAYLOAD"
+                            }
+                        ]
+                      },
+                      {
+                        "type":"web_url",
+                        "title":"Visit website ",
+                        "url":"http://www.techiediaries.com",
+                        "webview_height_ratio":"full"
+                    }
+                ]
             },
             {
-            "title":"Contact Me",
-            "type":"postback",
-            "payload":"CONTACT_INFO_PAYLOAD"
+              "locale":"zh_CN",
+              "composer_input_disabled":false
             }
-        ]
-        },
-        {
-        "type":"web_url",
-        "title":"Visit website ",
-        "url":"http://www.techiediaries.com",
-        "webview_height_ratio":"full"
-        }
-    ]
-    },
-    {
-    "locale":"zh_CN",
-    "composer_input_disabled":false
-    }
-]          
+          ]          
         };
         // Start the request
         request({
