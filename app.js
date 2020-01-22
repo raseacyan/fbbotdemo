29751e108f5f90ac081ac3665f75be05c2912530
Caesar <caesar@example.com>
2020-01-22 16:13:22 +0630

    test

--- app.js
+++ app.js
@@ -131,7 +131,14 @@ function handleMessage(sender_psid, received_message) {
     response = {
       "text": `Min Ga Lar Par Sint!`
     }
-  } else if (received_message.text) {    
+  }else if (received_message.text == "ni hao") {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `Hao Xie Xie. Ni Hao Mah!`
+    }
+  }
+   else if (received_message.text) {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
