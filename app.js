2b335a4a2c5d0f944787037c3d1ff20e8a9aedfc
Caesar <caesar@example.com>
2020-01-22 14:24:50 +0630

    test

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.post('/webhook', (req, res) => {
 
 app.get('/setup',function(req,res){
     setupGetStartedButton(res);
-    setupPersistentMenu(res); 
+    //setupPersistentMenu(res); 
 });
 
 
