bfbd3ac2f89c66521fe4423101ed6248717b3655
Caesar <caesar@example.com>
2020-01-22 11:03:32 +0630

    test

--- app.js
+++ app.js
@@ -219,8 +219,7 @@ function setupGetStartedButton(res){
 
         // Start the request
         request({
-            url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
-            "qs": { "access_token": PAGE_ACCESS_TOKEN },
+            url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+ PAGE_ACCESS_TOKEN,
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             form: messageData
