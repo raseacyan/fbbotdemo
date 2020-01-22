69e47b58b938a00b37d7a92bf750bb223154068a
Caesar <caesar@example.com>
2020-01-22 15:16:42 +0630

    test

--- app.js
+++ app.js
@@ -243,7 +243,7 @@ function setupPersistentMenu(res){
             "persistent_menu":[
     {
     "locale":"default",
-    "composer_input_disabled":true,
+    "composer_input_disabled":false,
     "call_to_actions":[
         {
         "title":"Info",
