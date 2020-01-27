712b4fa7ea0985dc3a9292d54f8184da87a98c98
Caesar <caesar@example.com>
2020-01-27 16:12:23 +0630

    Test

--- app.js
+++ app.js
@@ -237,7 +237,7 @@ function getUserProfile(sender_psid) {
     "method": "GET"
   }, (err, res, body) => {
     if (!err) {
-      retrun body;
+      return body;
     } else {
       console.error("Error:" + err);
     }
