fc60e9437aae9afdea2eb2dbfe8d3b83728a3028
Caesar <caesar@example.com>
2020-02-03 12:06:24 +0630

    test

--- app.js
+++ app.js
@@ -292,7 +292,7 @@ function notifyDelete(sender_psid){
 function getNumTasks(){
   itemsRef.once("value", function(snapshot) {
             
-           consol.log("getNumTasks", snapshot.size); 
+           console.log("getNumTasks", snapshot.size); 
             return snapshot.size;
 
            
